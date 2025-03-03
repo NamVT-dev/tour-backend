0f68aa339d7d1136b8d2ad1e0d0200f4ec3abf7b
NamVT <namvtfs1134@gmail.com>
2025-03-03 10:32:42 +0700

    Add authen + email

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const tourRoutes = require("./tourRoutes");
+const userRoutes = require("./userRoutes");
 
 const route = express.Router();
 
 route.use("/tours", tourRoutes);
+route.use("/user", userRoutes);
 
 module.exports = route;
